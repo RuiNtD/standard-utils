6b6711acb37c9905951b969ec380de0db82a9205
RuiNtD <RuiNtD@outlook.com>
2026-02-13 05:28:55 -0700

    Support Standard Schema 1.1.0

--- src/lib/async.ts
+++ src/lib/async.ts
@@ -8,8 +8,9 @@ import {
 export async function validate<T extends S>(
   schema: T,
   input: unknown,
+  options?: S.Options | undefined,
 ): Promise<Result<S.InferOutput<T>>> {
-  let result = schema["~standard"].validate(input);
+  let result = schema["~standard"].validate(input, options);
   if (result instanceof Promise) result = await result;
   return convertResult(result);
 }
@@ -17,8 +18,9 @@ export async function validate<T extends S>(
 export async function parse<T extends S>(
   schema: T,
   input: unknown,
+  options?: S.Options | undefined,
 ): Promise<S.InferOutput<T>> {
-  const result = await validate(schema, input);
+  const result = await validate(schema, input, options);
   if (result.issues) throw new SchemaError(result.issues);
   return result.value;
 }
@@ -27,26 +29,37 @@ export const safeParse = validate;
 export const decode = async <T extends S>(
   schema: T,
   input: S.InferInput<T>,
-): Promise<S.InferOutput<T>> => await parse(schema, input);
+  options?: S.Options | undefined,
+): Promise<S.InferOutput<T>> => await parse(schema, input, options);
 export const safeDecode = async <T extends S>(
   schema: T,
   input: S.InferInput<T>,
-): Promise<Result<S.InferOutput<T>>> => await safeParse(schema, input);
+  options?: S.Options | undefined,
+): Promise<Result<S.InferOutput<T>>> => await safeParse(schema, input, options);
 
 export class WrappedAsyncSchema<T extends S> {
   constructor(public readonly schema: T) {}
 
-  validate = (input: unknown): Promise<Result<S.InferOutput<T>>> =>
-    validate(this.schema, input);
+  validate = (
+    input: unknown,
+    options?: S.Options | undefined,
+  ): Promise<Result<S.InferOutput<T>>> => validate(this.schema, input, options);
 
-  parse = (input: unknown): Promise<S.InferOutput<T>> =>
-    parse(this.schema, input);
+  parse = (
+    input: unknown,
+    options?: S.Options | undefined,
+  ): Promise<S.InferOutput<T>> => parse(this.schema, input, options);
   safeParse = this.validate;
 
-  decode = (input: S.InferInput<T>): Promise<S.InferOutput<T>> =>
-    decode(this.schema, input);
-  safeDecode = (input: S.InferInput<T>): Promise<Result<S.InferOutput<T>>> =>
-    safeDecode(this.schema, input);
+  decode = (
+    input: S.InferInput<T>,
+    options?: S.Options | undefined,
+  ): Promise<S.InferOutput<T>> => decode(this.schema, input, options);
+  safeDecode = (
+    input: S.InferInput<T>,
+    options?: S.Options | undefined,
+  ): Promise<Result<S.InferOutput<T>>> =>
+    safeDecode(this.schema, input, options);
 }
 
 export const wrap = <T extends S>(schema: T): WrappedAsyncSchema<T> =>
--- src/lib/sync.ts
+++ src/lib/sync.ts
@@ -8,8 +8,9 @@ import {
 export function validate<T extends S>(
   schema: T,
   input: unknown,
+  options?: S.Options | undefined,
 ): Result<S.InferOutput<T>> {
-  const result = schema["~standard"].validate(input);
+  const result = schema["~standard"].validate(input, options);
   if (result instanceof Promise)
     throw new TypeError("Schema validation must be synchronous");
   return convertResult(result);
@@ -18,8 +19,9 @@ export function validate<T extends S>(
 export function parse<T extends S>(
   schema: T,
   input: unknown,
+  options?: S.Options | undefined,
 ): S.InferOutput<T> {
-  const result = validate(schema, input);
+  const result = validate(schema, input, options);
   if (result.issues) throw new SchemaError(result.issues);
   return result.value;
 }
@@ -28,42 +30,58 @@ export const safeParse = validate;
 export const decode = <T extends S>(
   schema: T,
   input: S.InferInput<T>,
-): S.InferOutput<T> => parse(schema, input);
+  options?: S.Options | undefined,
+): S.InferOutput<T> => parse(schema, input, options);
 export const safeDecode = <T extends S>(
   schema: T,
   input: S.InferInput<T>,
-): Result<S.InferOutput<T>> => safeParse(schema, input);
+  options?: S.Options | undefined,
+): Result<S.InferOutput<T>> => safeParse(schema, input, options);
 
 export function is<T extends S>(
   schema: T,
   input: unknown,
+  options?: S.Options | undefined,
 ): input is S.InferOutput<T> {
-  return validate(schema, input).success;
+  return validate(schema, input, options).success;
 }
 export function assert<T extends S>(
   schema: T,
   input: unknown,
+  options?: S.Options | undefined,
 ): asserts input is S.InferOutput<T> {
-  parse(schema, input);
+  parse(schema, input, options);
 }
 
 export class WrappedSyncSchema<T extends S> {
   constructor(public readonly schema: T) {}
 
-  validate = (input: unknown): Result<S.InferOutput<T>> =>
-    validate(this.schema, input);
+  validate = (
+    input: unknown,
+    options?: S.Options | undefined,
+  ): Result<S.InferOutput<T>> => validate(this.schema, input, options);
 
-  parse = (input: unknown): S.InferOutput<T> => parse(this.schema, input);
+  parse = (input: unknown, options?: S.Options | undefined): S.InferOutput<T> =>
+    parse(this.schema, input, options);
   safeParse = this.validate;
 
-  decode = (input: S.InferInput<T>): S.InferOutput<T> =>
-    decode(this.schema, input);
-  safeDecode = (input: S.InferInput<T>): Result<S.InferOutput<T>> =>
-    safeDecode(this.schema, input);
+  decode = (
+    input: S.InferInput<T>,
+    options?: S.Options | undefined,
+  ): S.InferOutput<T> => decode(this.schema, input, options);
+  safeDecode = (
+    input: S.InferInput<T>,
+    options?: S.Options | undefined,
+  ): Result<S.InferOutput<T>> => safeDecode(this.schema, input, options);
 
-  is = (input: unknown): input is S.InferOutput<T> => is(this.schema, input);
-  assert = (input: unknown): asserts input is S.InferOutput<T> =>
-    assert(this.schema, input);
+  is = (
+    input: unknown,
+    options?: S.Options | undefined,
+  ): input is S.InferOutput<T> => is(this.schema, input, options);
+  assert = (
+    input: unknown,
+    options?: S.Options | undefined,
+  ): asserts input is S.InferOutput<T> => assert(this.schema, input, options);
 }
 
 export const wrap = <T extends S>(schema: T): WrappedSyncSchema<T> =>
